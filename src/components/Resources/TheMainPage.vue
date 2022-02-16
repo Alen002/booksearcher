70a9bd9c271b8a2451b7c27ed2d8694c25ef2c08
Alen <alechkr@gmail.com>
2022-02-16 10:26:16 +0100

    added env

--- src/components/Resources/TheMainPage.vue
+++ src/components/Resources/TheMainPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div><h3>TheMainPage.vue</h3></div>
+  <div>
+    <h3>TheMainPage.vue {{ api }}</h3>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      api: process.env.VUE_APP_api,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
