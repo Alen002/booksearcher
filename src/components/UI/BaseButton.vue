ad89d53c343ade3284530c8ec082ffff8f9520f8
Alen <alechkr@gmail.com>
2022-02-16 14:58:57 +0100

    fix sass-loader

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button>CLICK ME</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-right: 20px;
+  color: white;
+}
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <h3>Home</h3>
-    <button>AXIOS</button>
+    <button @click="getData">AXIOS</button>
+    <base-button />
+    <pre>{{ results }}</pre>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import BaseButton from '../components/UI/BaseButton.vue';
 export default {
+  components: { BaseButton },
   data() {
     return {
       api: process.env.VUE_APP_api,
+      results: null,
     };
   },
   methods: {
-    getData() {},
+    getData() {
+      console.log('Start fetching___');
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=bradbury+inauthor:keyes&key=${this.api}`
+        )
+        .then((res) => res.data)
+        .then((data) => (this.results = data));
+    },
   },
 };
 </script>
