0c3434424b5f48cfcb4a5cb0c094ec12ded043a0
Alen <alechkr@gmail.com>
2022-02-18 21:25:21 +0100

    destructure payload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,18 @@
 
     <ul v-for="result in results" :key="result">
       <li>
-        <h3>Title:{{ result.volumeInfo.title }}</h3>
-        Authors:{{ result.volumeInfo.authors }}<br />
-        <p>Publisher {{ result.volumeInfo.publisher }}</p>
-        Description:{{ result.volumeInfo.description }}<br />
+        <h3>Title:{{ resulttitle }}</h3>
+        Authors:{{ result.authors }}<br />
+        <p>Publisher {{ result.publisher }}</p>
+        Description:{{ result.description }}<br />
       </li>
     </ul>
-    <h1>Check Results</h1>
-    <button @click="testing">TESTING</button>
-    <!--  <pre>{{ testingResults }}</pre> -->
-    <h3>REST</h3>
-    <pre>{{ rest }}</pre>
-    <h3>ITEMS</h3>
-    <pre>{{ items }}</pre>
   </div>
 </template>
 
 <script>
-import test from '../assets/test.js';
+import testObject from '../assets/test.js';
+
 import axios from 'axios';
 import BaseButton from '../components/UI/BaseButton.vue';
 import BaseSearch from '../components/UI/BaseSearch.vue';
@@ -32,38 +26,38 @@ export default {
   data() {
     return {
       api: process.env.VUE_APP_api,
-      /* results: [{ test: 121 }, { test: 45454545454 }], */
       results: '',
       endpoint: 'https://www.googleapis.com/books/v1/volumes',
       queryField: '',
-      jsonFile: test,
-      testingResults: '',
-      items: '',
-      rest: '',
+      jsonFile: testObject,
     };
   },
   methods: {
-    testing() {
-      /* this.testingResults = this.jsonFile.test.items.map((e) => e); */
-      const { items } = this.jsonFile.test;
-      this.testingResults = { ...this.jsonFile.test };
-      // How to manipulate data
-      this.items = items;
-    },
     enteredData(e) {
       this.queryField = e;
     },
     getData() {
-      console.log('Start fetching___');
       axios
         .get(
-          `${this.endpoint}?q=${this.queryField}&maxResults=30&keyes&key=${this.api}`
+          `${this.endpoint}?q=${this.queryField}&maxResults=3&keyes&key=${this.api}`
         )
         .then((res) => res.data)
-        /* .then((data) => (this.results = data)); */
         .then((data) => {
-          this.results = data.items;
+          destructureData(data);
         });
+
+      const destructureData = (e) => {
+        const { items } = e;
+        this.results = items.map(
+          ({ volumeInfo: { title, authors, publisher, description } }) => ({
+            title,
+            authors,
+            publisher,
+            description,
+          })
+        );
+        console.log(this.results);
+      };
     },
     clearData() {
       this.results = '';
