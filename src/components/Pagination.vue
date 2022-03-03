50d34e3d739921cbddf24b63a133028a16d4c8dd
Alen <alechkr@gmail.com>
2022-03-04 00:02:42 +0100

    add counter methods for pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,21 +1,53 @@
 <template>
   <div>PAGINATION</div>
   <h3>{{ pageItems }}</h3>
-  <div>
-    <base-button buttonTitle="Back" />
-    <base-button buttonTitle="Forth" />
+  <div class="pagination-layout">
+    <base-button
+      buttonTitle="Back"
+      @emitBaseButton="paginateCounter('decrease')"
+    />
+    <h2>{{ counter }}</h2>
+    <base-button
+      buttonTitle="Forth"
+      @emitBaseButton="paginateCounter('increase')"
+    />
   </div>
 </template>
 
 <script>
 import BaseButton from './UI/BaseButton.vue';
 export default {
+  data() {
+    return { counter: 0 };
+  },
   components: { BaseButton },
   props: ['pageItems'],
   methods: {
-    paginationCounter() {},
+    paginateCounter(clicked) {
+      if (
+        clicked === 'increase' &&
+        this.pageItems > 0 &&
+        this.counter < this.pageItems
+      ) {
+        this.counter += 1;
+      } else if (clicked === 'decrease' && this.counter > 0) {
+        this.counter -= 1;
+      }
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.pagination-layout {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 20%;
+  background-color: red;
+}
+
+h2 {
+  background-color: green;
+}
+</style>
